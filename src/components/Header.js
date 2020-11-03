7e46ec043d67535c21e332b28824daa2df9d1fea
adam-larosa <adam.larosa@flatironschool.com>
2020-11-02 22:39:21 -0800

    Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
-class Header extends Component {
-  render() {
-    return (
-      <header>
-        <h1 className="text-center">Flatiron Stock Exchange</h1>
-      </header>
-    );
-  }
+import React from 'react';
+const Header = () => {
+  return (
+    <header>
+      <h1 className="text-center">Flatiron Stock Exchange</h1>
+    </header>
+  );
 }
 export default Header;
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Stock from '../components/Stock'
-class PortfolioContainer extends Component {
-  render() {
-    const { folio, removeClick } = this.props
-    return (
-      <div>
-        <h2>My Portfolio</h2>
-          {
-            folio.map(stock => {
-              return (
-                <div key={stock.id} onClick={() => removeClick(stock)}>
-                  <Stock stock={stock} />
-                </div>
-              )
-            })
-            //render your portfolio stocks here
-          }
-      </div>
-    );
-  }
+const PortfolioContainer = props => {
+  const { folio, removeClick } = props
+  return (
+    <div>
+      <h2>My Portfolio</h2>
+        {
+          folio.map(stock => {
+            return (
+              <div key={stock.id} onClick={() => removeClick(stock)}>
+                <Stock stock={stock} />
+              </div>
+            )
+          })
+          //render your portfolio stocks here
+        }
+    </div>
+  );
 }
 export default PortfolioContainer;
