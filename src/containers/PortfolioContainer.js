59645c526624275d3f4d421893a1cf6aa6baf97f
adam-larosa <adam.larosa@flatironschool.com>
2020-11-02 22:33:06 -0800

    Done

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 class PortfolioContainer extends Component {
   render() {
+    const { folio, removeClick } = this.props
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            this.props.folio.map(stock => {
+            folio.map(stock => {
               return (
-                <div key={stock.id} onClick={() => this.props.removeClick(stock)}>
+                <div key={stock.id} onClick={() => removeClick(stock)}>
                   <Stock stock={stock} />
                 </div>
               )
